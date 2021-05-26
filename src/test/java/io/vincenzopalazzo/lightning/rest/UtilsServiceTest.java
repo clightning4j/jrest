b187299a6feb252eaa99ad38423d5d61a60b30f6
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-05-26 13:44:57 +0200

    Fixed #4
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/test/java/io/vincenzopalazzo/lightning/rest/UtilsServiceTest.java
+++ src/test/java/io/vincenzopalazzo/lightning/rest/UtilsServiceTest.java
@@ -16,11 +16,26 @@ public class UtilsServiceTest extends AbstractServiceTest {
     @Test
     public void GET_getInfoNode() {
         try {
-            CLightningGetInfo getInfo = rpc.getInfo();
-            String jsonResult = JavalinJson.toJson(getInfo);
-            HttpResponse response = Unirest.get("/utility/getinfo").asString();
+            var getInfo = rpc.getInfo();
+            var jsonResult = JavalinJson.toJson(getInfo);
+            var response = Unirest.get("/utility/getinfo").asString();
 
-            LOGGER.debug("GET_getInfoNode response: " + response.getBody().toString());
+            LOGGER.debug("GET_getInfoNode response: " + response.getBody());
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).isEqualTo(jsonResult);
+        } catch (CLightningException exception) {
+            TestCase.fail(exception.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void GET_listFunds() {
+        try {
+            var getInfo = rpc.listFunds();
+            var jsonResult = JavalinJson.toJson(getInfo);
+            var response = Unirest.get("/utility/listfounds").asString();
+
+            LOGGER.debug("GET_listFunds response: " + response.getBody());
             assertThat(response.getStatus()).isEqualTo(200);
             assertThat(response.getBody()).isEqualTo(jsonResult);
         } catch (CLightningException exception) {
