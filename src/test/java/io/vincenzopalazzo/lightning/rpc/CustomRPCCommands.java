5f8c815b0c84b20c3d3879fa961c85778883d7fe
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-08-25 23:37:12 +0200

    skip test of plugins
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/test/java/io/vincenzopalazzo/lightning/rpc/CustomRPCCommands.java
+++ src/test/java/io/vincenzopalazzo/lightning/rpc/CustomRPCCommands.java
@@ -5,21 +5,38 @@ import io.vincenzopalazzo.lightning.rest.model.rpc.type.CLightningMetricOne;
 import io.vincenzopalazzo.lightning.rest.utils.rpc.CLightningCommand;
 import io.vincenzopalazzo.lightning.rest.utils.rpc.command.CLightningDiagnosticRPC;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import jrpc.clightning.CLightningRPC;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assume.assumeThat;
+
 public class CustomRPCCommands {
+
+  private AtomicBoolean enable;
 
   @Before
   public void init() {
+    enable = new AtomicBoolean(false);
+    var help = CLightningRPC.getInstance().help();
+    help.getHelpItems().parallelStream()
+            .forEach(
+                    it -> {
+                      if (it.getCommand().contains("diagnostic")) {
+                        enable.set(true);
+                      }
+                    });
     CLightningRPC.getInstance()
         .registerCommand(CLightningCommand.DIAGNOSTIC, new CLightningDiagnosticRPC());
   }
 
   @Test
   public void testMetricsPluginDiagnosticCmdOne() {
+    assumeThat(enable.get(), is(true));
     HashMap<String, Object> params = new HashMap<>();
     params.put("metrics_id", "1");
     CLightningDiagnostic diagnostic =
@@ -29,6 +46,7 @@ public class CustomRPCCommands {
 
   @Test
   public void testMetricsPluginDiagnosticCmdTwo() {
+    assumeThat(enable.get(), is(true));
     HashMap<String, Object> params = new HashMap<>();
     params.put("metrics_id", "1");
     CLightningDiagnostic diagnostic =
@@ -42,6 +60,7 @@ public class CustomRPCCommands {
 
   @Test
   public void testMetricsPluginDiagnosticCmdThree() {
+    assumeThat(enable.get(), is(true));
     HashMap<String, Object> params = new HashMap<>();
     params.put("metrics_id", "1,");
     CLightningDiagnostic diagnostic =
